c14acdf5b29f9e5ebca6fbc5583ef58b362971c7
Zhivko Shopov <zhishopov@gmail.com>
2025-02-19 05:25:35 +0000

    Creating and nesting components

--- demos/src/App.jsx
+++ demos/src/App.jsx
@@ -1,5 +1,14 @@
+function MyButton() {
+  return <button>My Button</button>;
+}
+
 function App() {
-  return <h1>My First React App</h1>;
+  return (
+    <>
+      <h1>Welcome to my app</h1>
+      <MyButton></MyButton>
+    </>
+  );
 }
 
 export default App;
